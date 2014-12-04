01d97701e4672934ee427fccac7e021a8028ab9a
poklj <poklj@poklj's-PC>
2014-12-04 07:23:58 -0400

    Init the Irc attempt

---
+++ Pybotirc/Pyboti.py
@@ -0,0 +1,11 @@
+#-------------------------------------------------------------------------------
+# Name:        Pybot(Integrated IRC)
+# Purpose:     To create Pybot with an integrated IRC
+#
+# Author:      kickguy223
+#
+# Created:     04/12/2014
+# Copyright:   (c) kickguy223 2014
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
